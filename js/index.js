84277954a306d6579e5d221a80896355b2d6a101
Graphic <graphic@SJH520-00M.local>
2019-03-27 17:30:42 -0400

    basic state functionality

--- js/index.js
+++ js/index.js
@@ -2,11 +2,16 @@
 const { useState } = React;
 
 function Channel(props) {
+    let [rgbNum, setRgb] = useState(props.rgb);
+    const increaseValue = () => setRgb(rgbNum + 1);
+    const decreaseValue = () => setRgb(rgbNum - 1);
+    const changeValue = ({target}) => setRgb(Number(target.value));
+
     return (
         <div class="channel">
-            <button type="button" class="btn up">+</button>
-            <input type="text" class="txt" value={props.rgb} />
-            <button type="button" class="btn down">-</button>
+            <button type="button" class="btn up" onClick={increaseValue}>+</button>
+            <input type="text" class="txt" value={rgbNum} onChange={changeValue} />
+            <button type="button" class="btn down" onClick={decreaseValue}>-</button>
         </div>
     );
 }
@@ -30,9 +35,9 @@ function Colour(props) {
 function Palette() {
     return (
       <ul class="palette">
-        <Colour red="255" green="0" blue="0" />
-        <Colour red="0" green="255" blue="0" />
-        <Colour red="0" green="0" blue="255" />
+        <Colour red={255} green={0} blue={0} />
+        <Colour red={0} green={255} blue={0} />
+        <Colour red={0} green={0} blue={255} />
       </ul>
     );
 }
