daebb5572e0371b53323d05ea6b6a0ac6e34a207
Graphic <graphic@SJH520-00M.local>
2019-04-03 16:29:25 -0400

    Working swatches

--- js/index.js
+++ js/index.js
@@ -5,33 +5,44 @@ const { useState } = React;
 
 // Component: controls a single Channel of each swatch (R, G or B)
 function Channel(props) {
-    let [rgbNum, setRgb] = useState(props.rgb);         // Defines rgbNum as a state variable
-    const increaseValue = () => setRgb(rgbNum + 1);     // Increase rgbNum by 1
-    const decreaseValue = () => setRgb(rgbNum - 1);     // Decrease rgbNum by 1
-    const changeValue = ({target}) => setRgb(Number(target.value));     // If the value in the box changes, update rgbNum
+    let rgbNum = props.rgb;         // Defines rgbNum
+    
+    const updateRgb = (channel) => {
+      if( channel >= 0 && channel <= 255 ) {
+        rgbNum = channel;
+        props.handleOnChange(rgbNum);
+      }
+    }
 
     return (
         <div class="channel">
-            <button type="button" class="btn up" onClick={increaseValue}>+</button>
-            <input type="text" class="txt" value={rgbNum} onChange={changeValue} />
-            <button type="button" class="btn down" onClick={decreaseValue}>-</button>
+            <button type="button" class="btn up" onClick={e => updateRgb(rgbNum + 10)}>+</button>
+            <input type="text" class="txt" value={rgbNum} onChange={e => updateRgb( e.target.value )} />
+            <button type="button" class="btn down" onClick={e => updateRgb(rgbNum - 10)}>-</button>
         </div>
     );
 }
 
 // Component: a Colour (swatch) row inside of a palette column
 function Colour(props) {
+
+    const [r, setR] = useState(props.red);
+    const [g, setG] = useState(props.green);
+    const [b, setB] = useState(props.blue);
+
+    console.log(`New colour is: rgb(${r},${g},${b})`)
+
     // Define a background-color for the swatch
     const myStyles = {
-        backgroundColor: `rgb(${props.red},${props.green},${props.blue})`
+        backgroundColor: `rgb(${r},${g},${b})`
     }
 
     return (
         <li class="colour" style={myStyles}>
           <div>rgb(</div>
-            <Channel rgb={props.red} />
-            <Channel rgb={props.green} />
-            <Channel rgb={props.blue} />
+            <Channel rgb={r} handleOnChange={setR} />
+            <Channel rgb={g} handleOnChange={setG} />
+            <Channel rgb={b} handleOnChange={setB} />
           <div>);</div>
         </li>
     );
